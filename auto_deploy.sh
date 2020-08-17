574ba89cc6d931cc54e5d1ac4b92a59cfc2912c7
nepsi <rapha.95870p@gmail.com>
2020-08-17 05:36:18 +0200

    add version auto_deploy

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-#version : 1.2
+version=1.4
 
 error_usage(){
     echo "ERROR : argument not found or to mutch!" >&2
     echo "Use: $0 [options]"
     echo -e "\033[1;31m -d\033[0m ( to run whit Dockercompose ) "
     echo -e "\033[1;31m -h\033[0m ( to run whit DockerHub ) "
+    echo -e "\033[1;31m -v\033[0m ( Show version ) "
     exit 1
 }
 
@@ -15,6 +16,7 @@ error_tenant(){
     echo "Use: $0 [options]"
     echo -e "\033[1;31m -d\033[0m ( to run whit Dockercompose ) "
     echo -e "\033[1;31m -h\033[0m ( to run whit DockerHub ) "
+    echo -e "\033[1;31m -v\033[0m ( Show version ) "
     exit 1
 }
 
@@ -22,18 +24,21 @@ if [[ $# -lt 1 || $# -gt 1 ]]; then
   error_usage
 fi
 
-if [[ $1 = "-d" ]]; then
+if [[ "$1" == "-d" ]]; then
 	result=1
-elif [[ $1 = "-h" ]]; then
+elif [[ "$1" == "-h" ]]; then
 		result=2
+    elif [[ "$1" == "-v" ]]; then
+            echo " $0  |  Version : $version "
+            exit
 		else
 			error_tenant
 fi
 
 which docker 
-sult=$?
+itis=$?
 #echo "$sult"
-if [[ "$sult" ]]; then
+if [[ "$itis" ]]; then
     #add repo dokcer ?
     sudo apt update
     sudo apt install docker-ce
@@ -61,7 +66,9 @@ if [[ -x $PWD/CTFd/prepare.sh ]]; then
 
         fi
 
-
+if [[ "$result" == "1" ]]; then
+    #statement
+fi
 
 
 # script for the choose , run dockercompose or whit dockerhub
