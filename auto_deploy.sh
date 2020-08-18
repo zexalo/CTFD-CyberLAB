dbc4306a4cf15038ae076a24bc30f62087bf3a42
nepsi <rapha.95870p@gmail.com>
2020-08-18 11:07:34 +0200

    update script

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -45,12 +45,9 @@ if [[ "$itis" ]]; then
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
     sudo apt update
-    sudo apt install docker-ce
+    sudo apt install docker
+    sudo apt install docker-compose
     dodo=$?
-fi
-
-if [[ "$dodo" ]]; then
-    
 fi
 
 #check befor ? 
