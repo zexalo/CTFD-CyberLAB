7cc509e27fdb282adbdfb95776da8c1834c14a23
nepsi <rapha.95870p@gmail.com>
2020-08-18 11:05:03 +0200

    update script

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -41,7 +41,16 @@ itis=$?
 if [[ "$itis" ]]; then
     #add repo dokcer ?
     sudo apt update
+    sudo apt-get install  curl apt-transport-https ca-certificates software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
     sudo apt install docker-ce
+    dodo=$?
+fi
+
+if [[ "$dodo" ]]; then
+    
 fi
 
 #check befor ? 
